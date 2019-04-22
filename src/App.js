b274c5900560c2548a6221afed37f04925338071
Harry Zhang <harryzhang100@gmail.com>
2019-04-22 16:25:47 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
   onButtonSubmit = () => { /**imageurl is from the Security Review
                             */
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://aqueous-everglades-55745.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type':'application/json'},
           body: JSON.stringify({
@@ -86,7 +86,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if(response) {
-         fetch('http://localhost:3000/image', {
+         fetch('https://aqueous-everglades-55745.herokuapp.com/image', {
            method: 'put',
            headers: {'Content-Type':'application/json'},
            body: JSON.stringify({
